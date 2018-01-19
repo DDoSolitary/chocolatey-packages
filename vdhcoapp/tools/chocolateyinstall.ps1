5f7362395f0234f94867fef705d396319cedaf2d
DDoSolitary <DDoSolitary@gmail.com>
2018-01-19 13:24:16 +0800

    Initial commit.

---
+++ vdhcoapp/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageArgs = @{
+	packageName = $Env:ChocolateyPackageName
+	url = 'https://github.com/mi-g/vdhcoapp/releases/download/v1.1.2/VdhCoAppSetup-1.1.2.exe'
+	fileType = 'exe'
+	silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+	softwareName = 'VdhCoApp*'
+	checksumType = 'sha512'
+	checksum = 'b30e3eda2fda166d6ec749734a17adfc206659e9b301ba24a265fb14cebb85161f72e9642a35a6087492f2762c1c57a657373dfd05772d0f0742ec410fb94706'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ winapioverride/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+	packageName = $Env:ChocolateyPackageName
+	unzipLocation = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+	url = 'http://jacquelin.potier.free.fr/exe/winapioverride32_bin.zip'
+	checksumType = 'sha512'
+	checksum = '19269de497f20100411d0e8e10a47945ab2a5db2a38bdbe5cd7976ec30c4ab0de198051bfc73c43a8681e43c0ea991279741281c62f04216cf9327df5696b92c'
+}
+
+Install-ChocolateyZipPackage @packageArgs
