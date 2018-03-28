376f1669b7a58f970d1e83a2cf62c473db224c0b
DDoSolitary <DDoSolitary@gmail.com>
2018-03-28 17:41:11 +0900

    Update winapioverride to v6.6.2.

--- winapioverride/tools/chocolateyInstall.ps1
+++ winapioverride/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 	unzipLocation = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 	url = 'http://jacquelin.potier.free.fr/exe/winapioverride32_bin.zip'
 	checksumType = 'sha512'
-	checksum = '19269de497f20100411d0e8e10a47945ab2a5db2a38bdbe5cd7976ec30c4ab0de198051bfc73c43a8681e43c0ea991279741281c62f04216cf9327df5696b92c'
+	checksum = '5c7d7c8b836b61c365028ebbbd44728053f3fb14f1e66e898c94dd748caf10f010a90f98ee6ba9a8cbda653f7f77aa3d5d6b8a2c846a3aea25f914f14f10030c'
 }
 
 Install-ChocolateyZipPackage @packageArgs
