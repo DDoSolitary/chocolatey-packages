74f61a7d0a4f1f932701e137d5c670f05dda17d0
DDoSolitary <DDoSolitary@gmail.com>
2018-05-17 00:11:41 +0800

    Update vdhcoapp to v1.2.0.

--- vdhcoapp/tools/chocolateyInstall.ps1
+++ vdhcoapp/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageArgs = @{
 	packageName = $Env:ChocolateyPackageName
-	url = 'https://github.com/mi-g/vdhcoapp/releases/download/v1.1.3/VdhCoAppSetup-1.1.3.exe'
+	url = 'https://github.com/mi-g/vdhcoapp/releases/download/v1.2.0/VdhCoAppSetup-1.2.0.exe'
 	fileType = 'exe'
 	silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 	softwareName = 'VdhCoApp*'
 	checksumType = 'sha512'
-	checksum = '72e69bfcdeec2286f941136bed5e477b78201a2df8287b791a84e423df1691d07932508f19890d89ab95f8491593aa6ef89a610f08e418bbcf9f1fa54f94c24f'
+	checksum = 'f46ecc04329d889e483f0bec9f172b8053334908fcf5444cec88024881872ee7b31b22cb1f1d85d16294a67f9adee70922c9ebfd446512d68fe6abec4f7fab56'
 }
 
 Install-ChocolateyPackage @packageArgs
