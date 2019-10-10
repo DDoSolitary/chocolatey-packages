c4145c73928b6e0d2d3ec4d0e45e61bf5eeadef8
stumbaumr <stumbaumr@yahoo.de>
2019-10-10 17:35:52 +0200

    ...already a bit safer

--- virtio-drivers/tools/chocolateyUninstall.ps1
+++ virtio-drivers/tools/chocolateyUninstall.ps1
@@ -2,4 +2,4 @@
 $infListPath = Join-Path $pkgDir inflist.txt
 Invoke-Expression "$(Join-Path $pkgDir 'blnsvr.exe') -u"
 Get-Content $infListPath | % { pnputil /delete-driver $_ }
-dir Cert:\LocalMachine\TrustedPublisher | ? subject -match 'Red Hat' | Remove-Item
+Get-ChildItem Cert:\LocalMachine\TrustedPublisher | ? subject -match 'CN="Red Hat, Inc.", O="Red Hat, Inc.", L=Raleigh, S=North Carolina, C=US' | Remove-Item
