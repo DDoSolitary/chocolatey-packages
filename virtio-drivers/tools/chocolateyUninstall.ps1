bae998b871b1c42141a429c56fddef52b1952c46
stumbaumr <stumbaumr@yahoo.de>
2019-10-10 17:25:04 +0200

    Remove the installed Certificates, that belong to Red Hat
    ...maybe store/extract the correct THUMBPRINT from the certificate

--- virtio-drivers/tools/chocolateyUninstall.ps1
+++ virtio-drivers/tools/chocolateyUninstall.ps1
@@ -2,3 +2,4 @@
 $infListPath = Join-Path $pkgDir inflist.txt
 Invoke-Expression "$(Join-Path $pkgDir 'blnsvr.exe') -u"
 Get-Content $infListPath | % { pnputil /delete-driver $_ }
+dir Cert:\LocalMachine\TrustedPublisher | ? subject -match 'Red Hat' | Remove-Item
