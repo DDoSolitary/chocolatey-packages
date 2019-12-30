876c8d9f77ba7c1c29821ccc1d96e6b8888ca205
DDoSolitary <DDoSolitary@gmail.com>
2019-12-30 10:45:29 +0800

    [opensc] v0.20.0

--- opensc/tools/chocolateyInstall.ps1
+++ opensc/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$packageArgs = @{
 	packageName = $Env:ChocolateyPackageName
-	url = 'https://github.com/OpenSC/OpenSC/releases/download/0.19.0/OpenSC-win32_0.19.0.msi'
+	url = 'https://github.com/OpenSC/OpenSC/releases/download/0.20.0/OpenSC-0.20.0_win32.msi'
 	fileType = 'msi'
 	silentArgs = '/qn /norestart'
 	checksumType = 'sha512'
-	checksum = 'ba834fecf68f9d15d8e6d0edc35e4f27b7b3f503f2f876f5ec5d29b7b533a0e9fb742e4cc467c4306da0c7c3e50c404abdf7524443b2bf3a6a1c8d0dde22df7e'
+	checksum = '7ea2a2ecfab534a75fdcb7f03024b012bca855acf4ca289ee1ede9ca7be480d6670ae5ca189b2a0f8de88392ad81ab5dc36163bff2944f84060dd94d7c3efaa3'
 }
 
 Install-ChocolateyPackage @packageArgs
 
 if ((Get-OsArchitectureWidth) -eq 64) {
-	$packageArgs.url = 'https://github.com/OpenSC/OpenSC/releases/download/0.19.0/OpenSC-win64_0.19.0.msi'
-	$packageArgs.checksum = '7282f1467119e9f56ba470b8a6de202bd55ea976e6f219cdfdbedeca2da6509ca6bad28df954fe8d88656e6de8fa088a86d243d73a27e6288ca3ef3778f5670e'
+	$packageArgs.url = 'https://github.com/OpenSC/OpenSC/releases/download/0.20.0/OpenSC-0.20.0_win64.msi'
+	$packageArgs.checksum = 'dbbe882abd294fd2d6e4c30c003d6a2c1b34d627b503651471ae405d1a38705d84839a72dcb1d56e211dffa8bf25d5f3b13b8d83e5c4e4dcc9ca01c0e8262c58'
 	Install-ChocolateyPackage @packageArgs
 }
