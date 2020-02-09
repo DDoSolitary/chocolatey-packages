3cd95e85a6fac068fc826ad80006716b5c456f1d
DDoSolitary <DDoSolitary@gmail.com>
2020-02-09 16:53:32 +0800

    Add back the incorrectly removed BOM.

--- virtio-drivers/tools/chocolateyUninstall.ps1
+++ virtio-drivers/tools/chocolateyUninstall.ps1
@@ -1,4 +1,4 @@
-$pkgDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+﻿$pkgDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $infListPath = Join-Path $pkgDir inflist.txt
 Invoke-Expression "$(Join-Path $pkgDir 'blnsvr.exe') -u"
 Get-Content $infListPath | % { pnputil /delete-driver $_ /force }
