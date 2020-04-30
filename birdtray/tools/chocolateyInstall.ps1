d4baa1d79bc181ed0e433446eb55e1277fa803e3
DDoSolitary <DDoSolitary@gmail.com>
2020-05-01 01:22:29 +0800

    [birdtray] v1.8.1

--- birdtray/tools/chocolateyInstall.ps1
+++ birdtray/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageArgs = @{
 	packageName = $Env:ChocolateyPackageName
-	url = 'https://github.com/gyunaev/birdtray/releases/download/1.8.0/Birdtray-1.8.0-Win-x86.exe'
-	url64bit = 'https://github.com/gyunaev/birdtray/releases/download/1.8.0/Birdtray-1.8.0-Win-x64.exe'
+	url = 'https://github.com/gyunaev/birdtray/releases/download/1.8.1/Birdtray-1.8.1-Win-x86.exe'
+	url64bit = 'https://github.com/gyunaev/birdtray/releases/download/1.8.1/Birdtray-1.8.1-Win-x64.exe'
 	fileType = 'exe'
 	silentArgs = '/allusers /S'
 	softwareName = 'Birdtray*'
 	checksumType = 'sha512'
-	checksum = '82421e9f3314f3207779d8615e6029d36263eb832d226273e67fce256a68717c49455124f089d68494d997741460de51384650affadf78149c8f94c70434a906'
-	checksum64 = 'e78b1eafcb299e728b0e12bfc0b0ba85c7edcd062cab8987eb4fd9f8b16592ff57ba8b1f8deda25e05e399f1812e8b2788713c055e48784199187988cc01d5c5'
+	checksum = '61648fc082bcf860925763ed2a24ec8ac3c1b421cbdec126fc05062d185b2ce3943299a1e098328ec7fb4abfbd6f353a9986b4d4e26baf3815cb949d49f913af'
+	checksum64 = '8439f8fb7d4b1bd34690dc3f8043c547e7009a2a6c70272ceeaa1cf6dddcef86545fc7455f3c25a8e6d5c82a80a4f136a90cc98f40bc33bf1b0cb54caa7abe45'
 }
 
 Install-ChocolateyPackage @packageArgs
