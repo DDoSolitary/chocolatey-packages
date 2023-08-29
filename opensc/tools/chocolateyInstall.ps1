34eaef6b817955f83c072df5c697871d130c29f1
FibreFoX <fibrefox@dynamicfiles.de>
2023-08-29 08:55:14 +0200

    opensc: v0.23.0

--- opensc/tools/chocolateyInstall.ps1
+++ opensc/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$packageArgs = @{
 	packageName = $Env:ChocolateyPackageName
-	url = 'https://github.com/OpenSC/OpenSC/releases/download/0.22.0/OpenSC-0.22.0_win32.msi'
+	url = 'https://github.com/OpenSC/OpenSC/releases/download/0.23.0/OpenSC-0.23.0_win32.msi'
 	fileType = 'msi'
 	silentArgs = '/qn /norestart'
 	checksumType = 'sha512'
-	checksum = '557f1f6e865d24b3b6ee2ef385ed715bd4533f88d5af4c1454bbffdd4d51c29855fa328a1dbfc35a464d9810ead9be199c24ee452e2f6f38d8ab9b9864f30819'
+	checksum = '634b20ad23be4492f23878f46a4963f1b2ee5470c4f2b808a356655585712551e5c705a2fa912777d2a3225f5eb77249038629c8f69f837fcf859378af91ec7b'
 }
 
 Install-ChocolateyPackage @packageArgs
 
 if ((Get-OsArchitectureWidth) -eq 64) {
-	$packageArgs.url = 'https://github.com/OpenSC/OpenSC/releases/download/0.22.0/OpenSC-0.22.0_win64.msi'
-	$packageArgs.checksum = '54790360cf072207a5b86710cf5d2d5808c4a1d3e94e89b0ed611370371afeec590de5573759fa12e19f10e35b3f396b77ab66791d66e6bc6d68ec24a6cc2d3c'
+	$packageArgs.url = 'https://github.com/OpenSC/OpenSC/releases/download/0.23.0/OpenSC-0.23.0_win64.msi'
+	$packageArgs.checksum = '520f0a7c7de99118307e27733524f993242b6fce85e8835740fa55c9d03a9a15294ef752bdfa033ddb79e8fee242c186e69d983422ceee08314f1e82ef03eca7'
 	Install-ChocolateyPackage @packageArgs
 }
